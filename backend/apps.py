7df3c97b49cd451a9a05df7afcabf61abc20b2f6
creepingshadows7 <creepingshadows7878@gmail.com>
2025-11-14 11:43:43 +0100

    Add user model and serializers for user registration and events management

--- backend/apps.py
+++ backend/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class Gotin1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gotin1'
+
+
+
+
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
+class User(AbstractUser):
+    """Extended user model for housemates"""
+    pass
 # Create your models here.
+class Events(models.Model):
+    """Model representing Events"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length= 1000)
+    address = models.CharField(max_length=255)
+    poster = models.ManyToManyField(User, related_name='posted by')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['-created_at']
+    
+    
